e6c21d6ecdc8c65035bed1ed8145e25feaac5f63
gergicsmilan <gergics.milan95@gmail.com>
2019-08-22 11:38:33 +0200

    created fancy shopping cart

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -37,7 +37,10 @@ public class Initializer implements ServletContextListener {
         productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
+
         Product productTry = new Product("Amazon Fire HD 9", 99, "USD", "Amazon's latest Fire HD 9 tablet is a great value for media consumption.", tablet, amazon);
+        //The pics. doesn't working properly rgt now bc of the id, so I had to create another pics with (4.id) just for test <we need to delete it in the end>
+        productDataStore.add(productTry);
 
         User firstUser = new User("firstUser");
         User secondUser = new User("secondUser");
