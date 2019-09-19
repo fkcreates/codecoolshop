137d629c1a99b93db82b18196611e5c9520355c9
Fruzsina Kiss <fruzsinakiss1@gmail.com>
2019-09-19 14:57:48 +0200

    titles and descriptions updated

--- src/main/sql/init_products.sql
+++ src/main/sql/init_products.sql
@@ -1,4 +1,4 @@
-INSERT INTO product_category (id, name, description, department) VALUES (1, 'Kitchen Mgmt', 'Essentials for your kitchen.', 'kitchen stuff');
+INSERT INTO product_category (id, name, description, department) VALUES (1, 'Guide to a successful PA', 'Essentials for your kitchen.', 'kitchen stuff');
 SELECT pg_catalog.setval('product_category_id_seq', 2, true);
 
 INSERT INTO supplier(name, description) VALUES ('Amazon2', 'They have everything you might need');
@@ -7,9 +7,9 @@ SELECT pg_catalog.setval('supplier_id_seq', 2, true);
 INSERT INTO buyer VALUES (1, 'First User');
 SELECT pg_catalog.setval('buyer_id_seq', 2, true);
 
-INSERT INTO product VALUES (1, 'Awesome cocktail glass', 'The curviest glass youve ever seen.', 35, 'USD', 1, 1);
-INSERT INTO product VALUES (2, 'Forkest fork', 'Pure silver fork that picks up any food you want it to. Simply the best.', 144, 'USD', 1, 1);
-INSERT INTO product VALUES (3, 'Pure plate', 'A very simple and elegant plate. Serve your dinner, lunch or even breakfast in it. You wont be disappointed, we promise.', 25, 'USD', 1, 1);
-INSERT INTO product VALUES (4, 'Bamboo cutlery', 'Cutlery set made of bamboo. Please take care of the environment so our children can live long. Even though Pandas die so we can make these. No worries, they look cool.', 48, 'USD', 1, 1);
-INSERT INTO product VALUES (5, 'Set of knives and forks', 'This set contains 4 butter knives and 4 forks. Theyll look like youve inherited them from your great-grandma. But we know its just a mainstream deal.', 300, 'USD', 1, 1);
+INSERT INTO product VALUES (1, 'WORKBOOK', 'This is not your enemy. We advise you to fill it out on your own because you will learn a lot during the process.', 500, 'USD', 1, 1);
+INSERT INTO product VALUES (2, 'PA practice', 'Kitchen mgmt, Network, Farm, Taxi.. you name it. Make the UML diagrams and implement the functions. Keep practicing.', 1500, 'USD', 1, 1);
+INSERT INTO product VALUES (3, 'Peer consultation', 'Gather your peers and discuss pratices & questions you are unsure of. This will get you further in your studying process. Listen carefully and ask if you are lost.', 300, 'USD', 1, 1);
+INSERT INTO product VALUES (4, 'Mentor consultation', 'Pay the price and learn from the wise.', 250, 'USD', 1, 1);
+INSERT INTO product VALUES (5, 'Personal Assessment', 'Test your wisdom, use everything that you have learned on your journey. We wish you luck!', 2000, 'USD', 1, 1);
 SELECT pg_catalog.setval('product_id_seq', 6, true);
